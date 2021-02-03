37a56f5bedf7263da777a3ccbffdf581d212eaae
Tom Aldcroft <taldcroft@gmail.com>
2021-02-03 06:36:55 -0500

    Update __main__.py

--- sparkles/__main__.py
+++ sparkles/__main__.py
@@ -1,4 +1,4 @@
-# See https://stackoverflow.com/questions/43393764/python-3-6-project-structure-leads-to-runtimewarning
+# See https://stackoverflow.com/questions/43393764/
 
 from .core import main
 main()
