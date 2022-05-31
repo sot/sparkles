97e572ccc50fca60a92870fcff724affa5228cdb
Tom Aldcroft <taldcroft@gmail.com>
2022-05-31 17:44:38 -0400

    Apply black formatting

--- sparkles/yoshi.py
+++ sparkles/yoshi.py
@@ -26,66 +26,81 @@ def get_yoshi_params_from_ocat(obsid, obs_date=None, web_ocat=True):
     else:
         ocat = get_ocat_local(obsid=obsid)
 
-    if obs_date is None and ocat['start_date'] is not numpy.ma.masked:
+    if obs_date is None and ocat["start_date"] is not numpy.ma.masked:
         # If obsid has a defined start_date then use that. Otherwise if obsid is
         # not assigned an LTS bin then start_date will be masked and we fall
         # through to CxoTime(None) which is NOW.
-        obs_date = CxoTime(ocat['start_date']).date
+        obs_date = CxoTime(ocat["start_date"]).date
     else:
         obs_date = CxoTime(obs_date).date
 
-    targ = {'obs_date': obs_date,
-            'detector': ocat['instr'],
-            'ra_targ': ocat['ra'],
-            'dec_targ': ocat['dec'],
-            'offset_y': ocat['y_off'],
-            'offset_z': ocat['z_off']}
+    targ = {
+        "obs_date": obs_date,
+        "detector": ocat["instr"],
+        "ra_targ": ocat["ra"],
+        "dec_targ": ocat["dec"],
+        "offset_y": ocat["y_off"],
+        "offset_z": ocat["z_off"],
+    }
 
     # Leaving focus offset as not-implemented
-    targ['focus_offset'] = 0
+    targ["focus_offset"] = 0
 
     # For sim offsets, leave default at 0 unless explicit in OCAT
     # OCAT entry is in mm
-    targ['sim_offset'] = 0
-    if ocat['z_sim'] is not numpy.ma.masked:
-        targ['sim_offset'] = ocat['z_sim'] * 397.7225924607
+    targ["sim_offset"] = 0
+    if ocat["z_sim"] is not numpy.ma.masked:
+        targ["sim_offset"] = ocat["z_sim"] * 397.7225924607
 
     # Could use get_target_aimpoint but that needs icxc if not on HEAD
     # and we don't need to care that much for future observations.
-    aimpoints = {'ACIS-I': (970.0, 975.0, 3),
-                 'ACIS-S': (210.0, 520.0, 7),
-                 'HRC-S': (2195.0, 8915.0, 2),
-                 'HRC-I': (7590.0, 7745.0, 0)}
-    chip_x, chip_y, chip_id = aimpoints[ocat['instr']]
-    targ.update({'chipx': chip_x, 'chipy': chip_y, 'chip_id': chip_id})
+    aimpoints = {
+        "ACIS-I": (970.0, 975.0, 3),
+        "ACIS-S": (210.0, 520.0, 7),
+        "HRC-S": (2195.0, 8915.0, 2),
+        "HRC-I": (7590.0, 7745.0, 0),
+    }
+    chip_x, chip_y, chip_id = aimpoints[ocat["instr"]]
+    targ.update({"chipx": chip_x, "chipy": chip_y, "chip_id": chip_id})
 
     # Nominal roll is not quite the same targ and aca but don't care.
-    targ['roll_targ'] = nominal_roll(ocat['ra'],
-                                     ocat['dec'],
-                                     obs_date)
+    targ["roll_targ"] = nominal_roll(ocat["ra"], ocat["dec"], obs_date)
 
     # Set dither from defaults and override if defined in OCAT
-    if ocat['instr'].startswith('ACIS'):
-        targ.update({'dither_y': 8, 'dither_z': 8})
+    if ocat["instr"].startswith("ACIS"):
+        targ.update({"dither_y": 8, "dither_z": 8})
     else:
-        targ.update({'dither_y': 20, 'dither_z': 20})
+        targ.update({"dither_y": 20, "dither_z": 20})
 
-    if ocat['dither'] == 'Y':
-        targ.update({'dither_y': ocat['y_amp'] * 3600,
-                    'dither_z': ocat['z_amp'] * 3600})
-    if ocat['dither'] == 'N':
-        targ.update({'dither_y': 0, 'dither_z': 0})
+    if ocat["dither"] == "Y":
+        targ.update(
+            {"dither_y": ocat["y_amp"] * 3600, "dither_z": ocat["z_amp"] * 3600}
+        )
+    if ocat["dither"] == "N":
+        targ.update({"dither_y": 0, "dither_z": 0})
 
     return targ
 
 
 def run_one_yoshi(
-    *, obsid,
-    detector, chipx, chipy, chip_id,
-    ra_targ, dec_targ, roll_targ,
-    offset_y, offset_z, sim_offset, focus_offset,
-    dither_y, dither_z,
-    obs_date, t_ccd, man_angle,
+    *,
+    obsid,
+    detector,
+    chipx,
+    chipy,
+    chip_id,
+    ra_targ,
+    dec_targ,
+    roll_targ,
+    offset_y,
+    offset_z,
+    sim_offset,
+    focus_offset,
+    dither_y,
+    dither_z,
+    obs_date,
+    t_ccd,
+    man_angle,
     **kwargs
 ):
     """
@@ -117,12 +132,23 @@ def run_one_yoshi(
     from proseco import get_aca_catalog
 
     params = convert_yoshi_to_proseco_params(
-        obsid, detector,
-        chipx, chipy, chip_id,
-        ra_targ, dec_targ, roll_targ,
-        offset_y, offset_z, sim_offset, focus_offset,
-        dither_y, dither_z,
-        obs_date, t_ccd, man_angle
+        obsid,
+        detector,
+        chipx,
+        chipy,
+        chip_id,
+        ra_targ,
+        dec_targ,
+        roll_targ,
+        offset_y,
+        offset_z,
+        sim_offset,
+        focus_offset,
+        dither_y,
+        dither_z,
+        obs_date,
+        t_ccd,
+        man_angle,
     )
 
     # Update or override params from yoshi for call to get_aca_catalog
@@ -134,26 +160,39 @@ def run_one_yoshi(
     q_aca = aca.att
 
     # Get values for report
-    report = {'ra_aca': q_aca.ra,
-              'dec_aca': q_aca.dec,
-              'roll_aca': q_aca.roll,
-              'n_critical': len(acar.messages == 'critical'),
-              'n_warning': len(acar.messages == 'warning'),
-              'n_caution': len(acar.messages == 'caution'),
-              'n_info': len(acar.messages == 'info'),
-              'P2': -np.log10(acar.acqs.calc_p_safe()),
-              'guide_count': acar.guide_count}
+    report = {
+        "ra_aca": q_aca.ra,
+        "dec_aca": q_aca.dec,
+        "roll_aca": q_aca.roll,
+        "n_critical": len(acar.messages == "critical"),
+        "n_warning": len(acar.messages == "warning"),
+        "n_caution": len(acar.messages == "caution"),
+        "n_info": len(acar.messages == "info"),
+        "P2": -np.log10(acar.acqs.calc_p_safe()),
+        "guide_count": acar.guide_count,
+    }
 
     return report
 
 
 def convert_yoshi_to_proseco_params(
     obsid,
-    detector, chipx, chipy, chip_id,
-    ra_targ, dec_targ, roll_targ,
-    offset_y, offset_z, sim_offset, focus_offset,
-    dither_y, dither_z,
-    obs_date, t_ccd, man_angle
+    detector,
+    chipx,
+    chipy,
+    chip_id,
+    ra_targ,
+    dec_targ,
+    roll_targ,
+    offset_y,
+    offset_z,
+    sim_offset,
+    focus_offset,
+    dither_y,
+    dither_z,
+    obs_date,
+    t_ccd,
+    man_angle,
 ):
     """
     Convert yoshi parameters to equivalent proseco arguments
@@ -188,15 +227,17 @@ def convert_yoshi_to_proseco_params(
         focus_offset = 0.0
 
     # Calculate dynamic offsets using the supplied temperature.
-    aca_offset_y, aca_offset_z = get_aca_offsets(detector,
-                                                 chip_id, chipx, chipy,
-                                                 obs_date, t_ccd)
+    aca_offset_y, aca_offset_z = get_aca_offsets(
+        detector, chip_id, chipx, chipy, obs_date, t_ccd
+    )
 
     # Get the ACA quaternion using target offsets and dynamic offsets.
     # Note that calc_aca_from_targ expects target offsets in degrees and obs is now in arcmin
-    q_aca = calc_aca_from_targ((ra_targ, dec_targ, roll_targ),
-                               (offset_y / 60.) + (aca_offset_y / 3600.),
-                               (offset_z / 60.) + (aca_offset_z / 3600.))
+    q_aca = calc_aca_from_targ(
+        (ra_targ, dec_targ, roll_targ),
+        (offset_y / 60.0) + (aca_offset_y / 3600.0),
+        (offset_z / 60.0) + (aca_offset_z / 3600.0),
+    )
 
     # Get keywords for proseco
     out = dict(
@@ -211,7 +252,7 @@ def convert_yoshi_to_proseco_params(
         focus_offset=focus_offset,
         n_acq=8,
         n_guide=5,
-        n_fid=3
+        n_fid=3,
     )
 
     return out
