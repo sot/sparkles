4f73e318c35880dc284c3f7dffa9f8d1f57bb557
Jean Connelly <jconnelly@cfa.harvard.edu>
2023-07-24 12:41:52 -0400

    Use fixtures for the shared data in test_find_er_catalog

--- sparkles/tests/test_find_er_catalog.py
+++ sparkles/tests/test_find_er_catalog.py
@@ -1,6 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import os
 import warnings
+import pytest
 
 import agasc
 import numpy as np
@@ -16,31 +17,55 @@ from sparkles.find_er_catalog import (
     get_guide_counts,
 )
 
+
 # Known tough field: PKS 0023-26 pointing
-ATT = Quat([0.20668099834, 0.23164729391, 0.002658888173, 0.9505868852])
-DATE = "2021-09-13"
-T_CCD = -8.0
+@pytest.fixture
+def ATT():
+    return Quat([0.20668099834, 0.23164729391, 0.002658888173, 0.9505868852])
+
+
+@pytest.fixture
+def DATE():
+    return "2021-09-13"
+
+
+@pytest.fixture
+def T_CCD():
+    return -8.0
+
 
 # Get initial catalog at the PKS 0023-26 attitude. Ignore the penalty limit for
 # this work.
-KWARGS = mod_std_info(
-    att=ATT,
-    t_ccd=T_CCD,
-    date=DATE,
-    n_guide=8,
-    n_fid=0,
-    obsid=99999,
-    t_ccd_penalty_limit=999,
-)
-ACA = get_aca_catalog(**KWARGS)
-DPITCHES, DYAWS = np.ogrid[-0.01:-3.5:4j, -3.1:3:3j]
-SUN_RA, SUN_DEC = Ska.Sun.position(ACA.date)
-ATTS = Ska.Sun.apply_sun_pitch_yaw(
-    ACA.att, pitch=DPITCHES, yaw=DYAWS, sun_ra=SUN_RA, sun_dec=SUN_DEC
-)
 
 
-def test_get_candidate_and_filter_stars():
+@pytest.fixture
+def KWARGS(ATT, T_CCD, DATE):
+    return mod_std_info(
+        att=ATT,
+        t_ccd=T_CCD,
+        date=DATE,
+        n_guide=8,
+        n_fid=0,
+        obsid=99999,
+        t_ccd_penalty_limit=999,
+    )
+
+
+@pytest.fixture
+def ACA(KWARGS):
+    return get_aca_catalog(**KWARGS)
+
+
+@pytest.fixture
+def ATTS(ACA):
+    DPITCHES, DYAWS = np.ogrid[-0.01:-3.5:4j, -3.1:3:3j]
+    SUN_RA, SUN_DEC = Ska.Sun.position(ACA.date)
+    return Ska.Sun.apply_sun_pitch_yaw(
+        ACA.att, pitch=DPITCHES, yaw=DYAWS, sun_ra=SUN_RA, sun_dec=SUN_DEC
+    )
+
+
+def test_get_candidate_and_filter_stars(ATT, T_CCD, DATE):
     stars = get_candidate_stars(ATT, T_CCD, date=DATE)
     stars = filter_candidate_stars_on_ccd(ATT, stars)
 
@@ -64,7 +89,7 @@ TEST_COLS = [
 ]
 
 
-def test_find_er_catalog_minus_2_pitch_bins():
+def test_find_er_catalog_minus_2_pitch_bins(ACA, ATTS):
     # Try it all for the bad field near PKS 0023-26
     acar, att_opts = find_er_catalog(ACA, ATTS, alg="pitch_bins")
     # import pprint; pprint.pprint(att_opts[TEST_COLS].pformat_all(), width=100)
@@ -129,7 +154,7 @@ def test_find_er_catalog_minus_2_pitch_bins():
     ]
 
 
-def test_find_er_catalog_minus_2_count_all():
+def test_find_er_catalog_minus_2_count_all(ACA, ATTS):
     acar, att_opts = find_er_catalog(ACA, ATTS, alg="count_all")
     # import pprint; pprint.pprint(att_opts[TEST_COLS].pformat_all(), width=100)
     assert acar is att_opts["acar"][8]
@@ -193,7 +218,7 @@ def test_find_er_catalog_minus_2_count_all():
     ]
 
 
-def test_find_er_catalog_minus_2_input_order():
+def test_find_er_catalog_minus_2_input_order(ACA, ATTS):
     acar, att_opts = find_er_catalog(ACA, ATTS, alg="input_order")
     # import pprint; pprint.pprint(att_opts[TEST_COLS].pformat_all(), width=100)
     assert acar is att_opts["acar"][8]
@@ -257,7 +282,7 @@ def test_find_er_catalog_minus_2_input_order():
     ]
 
 
-def test_find_er_catalog_fails():
+def test_find_er_catalog_fails(ATT, DATE, ATTS):
     """Test a catalog that will certainly fail at +10 degC"""
     kwargs = mod_std_info(
         att=ATT,
